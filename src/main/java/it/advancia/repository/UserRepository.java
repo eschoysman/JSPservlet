b9dd251f06c2bbf6333bd5f5d3028af161b4ea22
abstractconstruct <michele.zhou@advancia.it>
2023-10-06 12:10:26 +0200

    Aggiunto Model.User e Repository.UserRepository

---
+++ src/main/java/it/advancia/repository/UserRepository.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package it.advancia.repository;
+
+/**
+ *
+ * @author Lavoro
+ */
+public class UserRepository {
+    
+}
