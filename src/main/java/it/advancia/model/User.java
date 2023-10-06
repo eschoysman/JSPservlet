d5305e597fe7ccd11b52d36f0bdb79d0b08cb147
abstractconstruct <michele.zhou@advancia.it>
2023-10-06 12:14:11 +0200

    Aggiunto proprietà in model.User

--- src/main/java/it/advancia/model/User.java
+++ src/main/java/it/advancia/model/User.java
@@ -4,10 +4,39 @@
  */
 package it.advancia.model;
 
+import java.util.Date;
+
 /**
  *
  * @author Lavoro
  */
 public class User {
     
+    private String username;
+    private String password;
+    private Date birthDate;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
 }
