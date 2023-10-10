1208b02c0055b8bbfa32d85ec932876e44c1e6ab
abstractconstruct <michele.zhou@advancia.it>
2023-10-10 17:14:40 +0200

    aggiunto nuove proprietà ai log per gestire file

--- src/main/java/it/advancia/model/LogOperazioniANSC.java
+++ src/main/java/it/advancia/model/LogOperazioniANSC.java
@@ -4,6 +4,7 @@
  */
 package it.advancia.model;
 
+import java.io.InputStream;
 import java.util.Date;
 
 /**
@@ -17,8 +18,26 @@ public class LogOperazioniANSC {
     private long idOperazioneComune;
     private String idRiferimento;
     private String date;
+
+    public InputStream getFileStream() {
+        return fileStream;
+    }
+
+    public void setFileStream(InputStream fileStream) {
+        this.fileStream = fileStream;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
     private String time;
     private String note;
+    private InputStream fileStream;
+    private String fileName;
 
     public String getNote() {
         return note;
